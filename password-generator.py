05669e6215b3f3b762b5bb0c1675df0a7444f741
SeryiBaran <seryibaranminepy@yandex.ru>
2021-12-01 19:33:58 +0300

    Add 3 набора

--- password-generator.py
+++ password-generator.py
@@ -13,12 +13,16 @@ default_type = '1'
 default_col = 12
 simbols_no_repeat = list('abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789')
 simbols_repeat = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+simbols_no_repeat_spec = list('abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789!#')
+simbols_repeat_spec = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#')
+simbols_no_repeat_all = list('abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789!@#%"&*()_-+={}<>?|:[].~')
+simbols_repeat_all = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#%"&*()_-+={}<>?|:[].~')
 
 def input_type():
-    type_pswrd_loc = input('\n\nКакой набор символов использовать,\nс похожими символами (i, L, l, 1) или без похожих?\n(без похожих - 1)\n(с похожими - 2)\n(Можете оставить по умолчанию, нажав ENTER)\n: ')
+    type_pswrd_loc = input(u'Список наборов:\nabcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789\nabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\nabcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789!#\nabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#\nabcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789!@#%"&*()_-+={}<>?|:[].~\nabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#%"&*()_-+={}<>?|:[].~\n\n\n--------------------\nПрочитай внимательно!\nПохожие друг на друга - l, L, 1, I, i.\nВнимание! Не все сервисы/сайты/программы принимают пароли со спецсимволами!\nПароли с "!" и "#" работают во всех сервисах/сайтах/программах.\n--------------------\n\n\nКакой набор символов использовать?\n1. Нужен пароль без похожих букв\n2. Нужен пароль с похожими буквами\n3. Нужен пароль без похожих букв с "!" и "#"\n4. Нужен пароль с похожими буквами и с "!" и "#"\n5. Нужен пароль без похожих букв со всеми спец-символами\n6. Нужен пароль с похожими буквами со всеми спец-символами\n\n(Можете оставить по умолчанию 1, нажав ENTER)\n: ')
     return type_pswrd_loc
 def input_col():
-    col_pswrd_loc = input('\n\nКакой длины пароль сгенерировать?\n(Меньше 12 не рекомендую, можете оставить по умолчанию, нажав ENTER)\n: ')
+    col_pswrd_loc = input(f'\n\nКакой длины пароль сгенерировать?\n(Меньше {default_col} не рекомендую, можете оставить по умолчанию {default_col}, нажав ENTER)\n: ')
     return col_pswrd_loc
 
 
@@ -28,9 +32,17 @@ if type_pswrd == '':
     type_pswrd = default_type
 if type_pswrd == '1':
     simbols = simbols_no_repeat
-if type_pswrd == '2':
+elif type_pswrd == '2':
     simbols = simbols_repeat
-while type_pswrd != '1' and type_pswrd != '2':
+elif type_pswrd == '3':
+    simbols = simbols_no_repeat_spec
+elif type_pswrd == '4':
+    simbols = simbols_repeat_spec
+elif type_pswrd == '5':
+    simbols = simbols_no_repeat_all
+elif type_pswrd == '6':
+    simbols = simbols_repeat_all
+while type_pswrd != '1' and type_pswrd != '2' and type_pswrd != '3' and type_pswrd != '4' and type_pswrd != '5' and type_pswrd != '6':
     type_pswrd = input_type()
 col_pswrd = input_col()
 if col_pswrd == '':
