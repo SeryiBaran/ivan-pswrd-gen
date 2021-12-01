3c4acc65bf0af6a58ea04f2239cf3a9212e0d613
SeryiBaran <seryibaranminepy@yandex.ru>
2021-12-01 19:56:25 +0300

    delete comments in py

--- password-generator.py
+++ password-generator.py
@@ -1,12 +1,3 @@
-# Генератор паролей
-# Меньше 12 символов пароль слабый, ставьте больше
-
-# Набор символов с похожими друг на друга(i,I,1,l,L):
-# abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
-
-# Набор символов без похожих друг на друга букв (по умолчанию):
-# abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789
-
 import random
 
 default_type = '1'
