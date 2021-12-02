af6bfab65782293ecab7c22926cf935855ad9b56
SeryiBaran <seryibaranminepy@yandex.ru>
2021-12-02 09:30:14 +0300

    увеличил шанс ! и # в пароле

--- password-generator.py
+++ password-generator.py
@@ -1,16 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import random
 
 default_type = '1'
 default_col = 12
 simbols_no_repeat = list('abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789')
 simbols_repeat = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
-simbols_no_repeat_spec = list('abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789!#')
-simbols_repeat_spec = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#')
+simbols_no_repeat_spec = list('abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789!#!#!#!#!#')
+simbols_repeat_spec = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#!#!#!#!#')
 simbols_no_repeat_all = list('abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789!@#%"&*()_-+={}<>?|:[].~')
 simbols_repeat_all = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#%"&*()_-+={}<>?|:[].~')
 
 def input_type():
-    type_pswrd_loc = input(u'Список наборов:\nabcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789\nabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\nabcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789!#\nabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#\nabcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789!@#%"&*()_-+={}<>?|:[].~\nabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#%"&*()_-+={}<>?|:[].~\n\n\n--------------------\nПрочитай внимательно!\nПохожие друг на друга - l, L, 1, I, i.\nВнимание! Не все сервисы/сайты/программы принимают пароли со спецсимволами!\nПароли с "!" и "#" работают во всех сервисах/сайтах/программах.\n--------------------\n\n\nКакой набор символов использовать?\n1. Нужен пароль без похожих букв\n2. Нужен пароль с похожими буквами\n3. Нужен пароль без похожих букв с "!" и "#"\n4. Нужен пароль с похожими буквами и с "!" и "#"\n5. Нужен пароль без похожих букв со всеми спец-символами\n6. Нужен пароль с похожими буквами со всеми спец-символами\n\n(Можете оставить по умолчанию 1, нажав ENTER)\n: ')
+    type_pswrd_loc = input(u'Список наборов:\nabcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789\nabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\nabcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789!#!#!#!#!#\nabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#!#!#!#!#\nabcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789!@#%"&*()_-+={}<>?|:[].~\nabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#%"&*()_-+={}<>?|:[].~\n\n\n--------------------\nПрочитай внимательно!\nПохожие друг на друга - l, L, 1, I, i.\nВнимание! Не все сервисы/сайты/программы принимают пароли со спецсимволами!\nПароли с "!" и "#" работают во всех сервисах/сайтах/программах.\n--------------------\n\n\nКакой набор символов использовать?\n1. Нужен пароль без похожих букв\n2. Нужен пароль с похожими буквами\n3. Нужен пароль без похожих букв с "!" и "#"\n4. Нужен пароль с похожими буквами и с "!" и "#"\n5. Нужен пароль без похожих букв со всеми спец-символами\n6. Нужен пароль с похожими буквами со всеми спец-символами\n\n(Можете оставить по умолчанию 1, нажав ENTER)\n: ')
     return type_pswrd_loc
 def input_col():
     col_pswrd_loc = input(f'\n\nКакой длины пароль сгенерировать?\n(Меньше {default_col} не рекомендую, можете оставить по умолчанию {default_col}, нажав ENTER)\n: ')
